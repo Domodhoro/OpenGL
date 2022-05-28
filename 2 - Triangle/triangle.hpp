630f355f784bca4d9444ae62cc9b2557f9e3eb0d
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-28 18:38:23 -0300

    Update triangle.hpp

--- 2 - Triangle/triangle.hpp
+++ 2 - Triangle/triangle.hpp
@@ -8,26 +8,23 @@
 
 #include "shader.hpp"
 
-class Triangle
-{
+class Triangle {
+    unsigned int m_VAO = 0u, m_count = 0u;
+
+    std::unique_ptr<Shader> m_shader;
+    std::vector<glm::vec3> m_vertexCoords, m_colorCoords;
+
+    void mesh(), setup();
+    
+    
 public:
     Triangle(const char*, const char*);
-
     Triangle(const Triangle&) = delete;
     Triangle& operator=(const Triangle&) = delete;
 
     ~Triangle();
 
     void draw(glm::mat4&) const;
-
-private:
-    unsigned int m_VAO = 0u, m_count = 0u;
-
-    std::unique_ptr<Shader> m_shader;
-    std::vector<glm::vec3> m_vertexCoords, m_colorCoords;
-
-    void mesh();
-    void setup();
 };
 
 #endif
