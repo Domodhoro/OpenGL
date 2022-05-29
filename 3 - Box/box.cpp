a4b31a8c1e836db4c41d3ce80049c6db1092d834
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-29 14:36:36 -0300

    Update box.cpp

--- 3 - Box/box.cpp
+++ 3 - Box/box.cpp
@@ -8,9 +8,7 @@
 
 #include "box.hpp"
 
-Box::Box(const unsigned int& texture)
-    : m_shader(std::make_unique<Shader>("boxVertex.glsl", "boxFragment.glsl")), m_texture(texture)
-{
+Box::Box(const unsigned int& texture) : m_shader(std::make_unique<Shader>("boxVertex.glsl", "boxFragment.glsl")), m_texture(texture) {
     m_count = mesh(m_vertexCoords, m_textureCoords);
 
     glGenVertexArrays(1, &m_VAO);
@@ -26,37 +24,34 @@ Box::Box(const unsigned int& texture)
 
     m_vertexCoords.clear();
 
-	glGenBuffers(1, &textureBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, textureBuffer);
-	glBufferData(GL_ARRAY_BUFFER, m_textureCoords.size() * sizeof(glm::vec2), &m_textureCoords.at(0), GL_STATIC_DRAW);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(glm::vec2), (void*)(0 * sizeof(float)));
+    glGenBuffers(1, &textureBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, textureBuffer);
+    glBufferData(GL_ARRAY_BUFFER, m_textureCoords.size() * sizeof(glm::vec2), &m_textureCoords.at(0), GL_STATIC_DRAW);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(glm::vec2), (void*)(0 * sizeof(float)));
 
-	m_textureCoords.clear();
+    m_textureCoords.clear();
 
-    if(!m_VAO)
-    {
+    if (!m_VAO) {
         throw std::runtime_error("Falha ao criar VAO.");
     }
 }
 
-Box::~Box()
-{
+Box::~Box() {
     glDeleteProgram(m_shader->use());
     glDeleteVertexArrays(1, &m_VAO);
 }
 
-void Box::draw(glm::mat4& projectionMatrix, glm::mat4& viewMatrix) const
-{
+void Box::draw(glm::mat4& projectionMatrix, glm::mat4& viewMatrix) const {
     glCullFace(GL_FRONT);
 
     glUseProgram(m_shader->use());
     glBindTexture(GL_TEXTURE_2D, m_texture);
-	glBindVertexArray(m_VAO);
+    glBindVertexArray(m_VAO);
 
-	glm::mat4 modelMatrix = glm::mat4(1.0f);
+    glm::mat4 modelMatrix = glm::mat4(1.0f);
 
-	modelMatrix = glm::rotate(modelMatrix, glm::radians(static_cast<float>(45.0f * glfwGetTime())), glm::vec3(1.0f));
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(static_cast<float>(45.0f * glfwGetTime())), glm::vec3(1.0f));
     modelMatrix = glm::translate(modelMatrix, glm::vec3(-0.5f));
 
     m_shader->setUniformMatrix("model", modelMatrix);
@@ -67,8 +62,7 @@ void Box::draw(glm::mat4& projectionMatrix, glm::mat4& viewMatrix) const
     glBindVertexArray(0);
 }
 
-unsigned int Box::mesh(std::vector<glm::vec3>& m_vertexCoords, std::vector<glm::vec2>& m_textureCoords)
-{
+unsigned int Box::mesh(std::vector<glm::vec3>& m_vertexCoords, std::vector<glm::vec2>& m_textureCoords) {
     m_vertexCoords.emplace_back(glm::vec3(0.0f, 0.0f, 0.0f));
     m_vertexCoords.emplace_back(glm::vec3(1.0f, 0.0f, 0.0f));
     m_vertexCoords.emplace_back(glm::vec3(1.0f, 1.0f, 0.0f));
