13777a8b4a24500312c5e59b6ddc38611f5d19cf
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-29 15:58:48 -0300

    Update shader.cpp

--- 3 - Box/shader.cpp
+++ 3 - Box/shader.cpp
@@ -9,24 +9,23 @@
 #include "shader.hpp"
 
 Shader::Shader(const char* vertexPath, const char* fragmentPath) : m_vertex(compileVertex(read(vertexPath).c_str())), m_fragment(compileFragment(read(fragmentPath).c_str())) {
-	m_shader = glCreateProgram();
+    m_shader = glCreateProgram();
 
-	glAttachShader(m_shader, m_vertex);
-	glAttachShader(m_shader, m_fragment);
-	glLinkProgram(m_shader);
+    glAttachShader(m_shader, m_vertex);
+    glAttachShader(m_shader, m_fragment);
+    glLinkProgram(m_shader);
 
-	int success = 0;
-	glGetProgramiv(m_shader, GL_LINK_STATUS, &success);
+    int success = 0;
+    glGetProgramiv(m_shader, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetProgramInfoLog(m_shader, 512, nullptr, infoLog);
 
-	if (!success) {
-            char infoLog[512];
-            glGetProgramInfoLog(m_shader, 512, nullptr, infoLog);
+        throw std::runtime_error("Falha ao compilar shader: " + std::string(infoLog));
+    }
 
-            throw std::runtime_error("Falha ao compilar shader: " + std::string(infoLog));
-	}
-
-	glDeleteShader(m_vertex);
-	glDeleteShader(m_fragment);
+    glDeleteShader(m_vertex);
+    glDeleteShader(m_fragment);
 }
 
 Shader::~Shader() {
@@ -38,40 +37,38 @@ unsigned int& Shader::use() {
 }
 
 unsigned int Shader::compileVertex(const char* vertexPath) {
-	unsigned int m_vertex = glCreateShader(GL_VERTEX_SHADER);
+    unsigned int m_vertex = glCreateShader(GL_VERTEX_SHADER);
 
-	glShaderSource(m_vertex, 1, &vertexPath, 0);
-	glCompileShader(m_vertex);
+    glShaderSource(m_vertex, 1, &vertexPath, 0);
+    glCompileShader(m_vertex);
 
-	int success = 0;
-	glGetShaderiv(m_vertex, GL_COMPILE_STATUS, &success);
+    int success = 0;
+    glGetShaderiv(m_vertex, GL_COMPILE_STATUS, &success);
+    if (!success) {
+       char infoLog[512];
+       glGetShaderInfoLog(m_vertex, 512, nullptr, infoLog);
 
-	if (!success) {
-            char infoLog[512];
-            glGetShaderInfoLog(m_vertex, 512, nullptr, infoLog);
+       throw std::runtime_error("Falha ao compilar vertex: " + std::string(infoLog));
+    }
 
-            throw std::runtime_error("Falha ao compilar vertex: " + std::string(infoLog));
-	}
-
-	return m_vertex;
+    return m_vertex;
 }
 
 unsigned int Shader::compileFragment(const char* fragmentPath) {
-	unsigned int m_fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int m_fragment = glCreateShader(GL_FRAGMENT_SHADER);
 
-	glShaderSource(m_fragment, 1, &fragmentPath, 0);
-	glCompileShader(m_fragment);
+    glShaderSource(m_fragment, 1, &fragmentPath, 0);
+    glCompileShader(m_fragment);
+    int success = 0;
+    glGetShaderiv(m_fragment, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        char infoLog[512];
+        glGetShaderInfoLog(m_fragment, 512, nullptr, infoLog);
 
-	int success = 0;
-	glGetShaderiv(m_fragment, GL_COMPILE_STATUS, &success);
-	if (!success) {
-            char infoLog[512];
-            glGetShaderInfoLog(m_fragment, 512, nullptr, infoLog);
+        throw std::runtime_error("Falha ao compilar fragment: " + std::string(infoLog));
+    }
 
-            throw std::runtime_error("Falha ao compilar fragment: " + std::string(infoLog));
-	}
-
-	return m_fragment;
+    return m_fragment;
 }
 
 void Shader::setUniformMatrix(const char* ID, const glm::mat4& matrix) const {
