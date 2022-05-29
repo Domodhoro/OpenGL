e32b4ae99d15c435362529b6f5cdaa49fb9fd8e7
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-29 14:37:29 -0300

    Update box.hpp

--- 3 - Box/box.hpp
+++ 3 - Box/box.hpp
@@ -8,27 +8,22 @@
 
 #include "shader.hpp"
 
-class Box
-{
+class Box {
+    unsigned int m_texture = 0u, m_VAO = 0u, m_count = 0u, mesh(std::vector<glm::vec3>&, std::vector<glm::vec2>&);
+
+    std::vector<glm::vec3> m_vertexCoords;
+    std::vector<glm::vec2> m_textureCoords;
+
+    std::unique_ptr<Shader> m_shader;
+    
 public:
     Box(const unsigned int&);
-
     Box(const Box&) = delete;
     Box& operator=(const Box&) = delete;
 
     ~Box();
 
     void draw(glm::mat4&, glm::mat4&) const;
-
-private:
-    unsigned int m_texture = 0u, m_VAO = 0u, m_count = 0u;
-
-    std::vector<glm::vec3> m_vertexCoords;
-    std::vector<glm::vec2> m_textureCoords;
-
-    std::unique_ptr<Shader> m_shader;
-
-    unsigned int mesh(std::vector<glm::vec3>&, std::vector<glm::vec2>&);
 };
 
 #endif
