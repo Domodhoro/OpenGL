c0ff41bc657dd6c7bb862867156eb126e9618aae
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-29 14:40:07 -0300

    Update camera.hpp

--- 3 - Box/camera.hpp
+++ 3 - Box/camera.hpp
@@ -3,20 +3,17 @@
 
 #include <glm/glm.hpp>
 
-class Camera
-{
+class Camera {
+    float m_fov = 60.0f, m_aspect = 1.0f, m_nearPlane = 0.1f, m_farPlane = 10.0f;
+
+    glm::vec3 m_position = glm::vec3(0.0f), m_front = glm::vec3(0.0f, 0.0f, 1.0f), m_up = glm::vec3(0.0f, 1.0f, 0.0f);
+	
 public:
     Camera(const float, const float, const float, const float);
 
     void setPosition(const glm::vec3);
 
-	glm::mat4 getProjectionMatrix() const;
-	glm::mat4 getViewMatrix() const;
-
-private:
-    float m_fov = 60.0f, m_aspect = 1.0f, m_nearPlane = 0.1f, m_farPlane = 10.0f;
-
-    glm::vec3 m_position, m_front, m_up;
+    glm::mat4 getProjectionMatrix() const, getViewMatrix() const;
 };
 
 #endif
