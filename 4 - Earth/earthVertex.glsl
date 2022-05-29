e0517d75f355a2044d8a22cc7cea53b51fe466bd
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-29 15:07:00 -0300

    Update earthVertex.glsl

--- 4 - Earth/earthVertex.glsl
+++ 4 - Earth/earthVertex.glsl
@@ -6,9 +6,8 @@ layout(location = 1) in vec2 textureCoords;
 uniform mat4 model, view, projection;
 out highp vec2 Texture;
 
-void main()
-{
-	gl_Position = projection * view * model * vec4(vertexCoords, 1.0f);
-	Texture = textureCoords;
+void main() {
+    gl_Position = projection * view * model * vec4(vertexCoords, 1.0f);
+    Texture = textureCoords;
 }
 
