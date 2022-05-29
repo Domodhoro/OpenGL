8defcb618d72fc25367f9ccd1a14bf3af2ee13bb
Guilherme <100392814+Domodhoro@users.noreply.github.com>
2022-05-29 14:38:46 -0300

    Update camera.cpp

--- 3 - Box/camera.cpp
+++ 3 - Box/camera.cpp
@@ -2,25 +2,16 @@
 
 #include "camera.hpp"
 
-Camera::Camera(const float fov, const float aspect, const float nearPlane, const float farPlane)
-    : m_fov(fov), m_aspect(aspect), m_nearPlane(nearPlane), m_farPlane(farPlane)
-{
-    m_position = glm::vec3(0.0f);
-    m_front = glm::vec3(0.0f, 0.0f, 1.0f);
-    m_up = glm::vec3(0.0f, 1.0f, 0.0f);
-}
+Camera::Camera(const float fov, const float aspect, const float nearPlane, const float farPlane) : m_fov(fov), m_aspect(aspect), m_nearPlane(nearPlane), m_farPlane(farPlane) {}
 
-void Camera::setPosition(const glm::vec3 position)
-{
+void Camera::setPosition(const glm::vec3 position) {
     m_position = position;
 }
 
-glm::mat4 Camera::getProjectionMatrix() const
-{
+glm::mat4 Camera::getProjectionMatrix() const {
 	return glm::perspective(glm::radians(m_fov), m_aspect, m_nearPlane, m_farPlane);
 }
 
-glm::mat4 Camera::getViewMatrix() const
-{
+glm::mat4 Camera::getViewMatrix() const {
 	return glm::lookAt(m_position, m_position + m_front, m_up);
 }
